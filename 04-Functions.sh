0c60959c0e693d27edfc601110aa3f00f3cadd87
raghu2671 <raghu.gadasu2017@gmail.com>
2024-12-27 19:00:10 +0530

    variables demo

--- 04-Functions.sh
+++ 04-Functions.sh
@@ -1,8 +1,10 @@
 #declaring a fuction--> funtionname() , open close parenthesis & flowerbraces
-
+#If you assign a name to a commands , it is called as a function
 abc(){
   echo Hello World
+  echo hello universe
+  echo hello galaxy
 }
 
-#How to access function just give function name
+#How to access function just give function names
 abc
